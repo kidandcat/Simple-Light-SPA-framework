43c60d9e76a68aba5e594fbbb446c1732985999d
jairo <kidandcat@gmail.com>
2015-12-10 12:57:28 +0100

    Some initial comments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,12 @@
 UImanager.on('page', function(e){
+  if(e.page == 'index'){
+    UImanager.render('index');
+  }
   console.log('new page: ');
   console.log(e.page);
   console.log(e.type);
   console.log(e);
 });
+
+/* UImanager.render('mariano') */
+/* UImanager.render('deux') */
